54e6588825a67d26ddec9c0bde70c85900feed2c
Szczepan Wąs <szczepan.was1@gmail.com>
2016-06-18 10:44:51 +0200

    Update Main.php

--- scr/vip/Main.php
+++ scr/vip/Main.php
@@ -18,17 +18,21 @@ class Main extends PluginBase{
   public function onCommand(CommandSender $sender, Command $command, $label, array $args){	
 			if(strtolower($command->getName('vip'))) {
 				if(empty($args)) {
-					$sender->sendMessage("§7[ ========== §vip§7 ========== ]");
-					$sender->sendMessage("§7meheejj?");
-					$sender->sendMessage("§o§a  * lewej");
-					$sender->sendMessage("§o§a  * dher:");
-					$sender->sendMessage("§7 djejs:");
-					$sender->sendMessage("§o§a  * jdjshd");
-					$sender->sendMessage("§7[ ========== §vip§7 ========== ]");
+					$sender->sendMessage("§7[ ========== §cvip§7 ========== ]");
+					$sender->sendMessage("text1");
+					$sender->sendMessage("text2");
+					$sender->sendMessage("text3");
+					$sender->sendMessage("text4");
+					$sender->sendMessage("text5");
+					$sender->sendMessage("§7[ ========== §cvip§7 ========== ]");
 					return true;
 				}
 					 if($args[0] == "kup") {
-                                        $sender->sendMessage("jejejejejrhrjrjrjrjr");
+                                        $sender->sendMessage("text1");
+                                        $sender->sendMessage("text2");
+                                        $sender->sendMessage("text3");
+                                        $sender->sendMessage("text4");
+                                        $sender->sendMessage("text5");
 	}
 }
 }
