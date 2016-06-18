077a05fb7444e4c3a47b9a01fa783a00cb343c71
Szczepan Wąs <szczepan.was1@gmail.com>
2016-06-18 09:05:31 +0200

    Create Main.php

---
+++ scr/vip/Main.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace vip;
+
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\event\Listener;
+use pocketmine\inventory\Inventory;
+use pocketmine\item\Item;
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase{
+	public function onEnable(){
+		$this->getLogger()->info("CobblexKup Zaladowane");
+		}
+
+  public function onCommand(CommandSender $sender, Command $command, $label, array $args){	
+			if(strtolower($command->getName('cobblex'))) {
+				if(empty($args)) {
+					$sender->sendMessage("§7[ ========== §eCOBBLEX§7 ========== ]");
+					$sender->sendMessage("§7Co to jest §eCOBBLEX?");
+					$sender->sendMessage("§o§a  * Jest to blok ktory po postawieniu daje losowe");
+					$sender->sendMessage("§o§a  * itemy:");
+					$sender->sendMessage("§7Aby kupic wpisz:");
+					$sender->sendMessage("§o§a  * /cobblex kup §6- §o§cKoszt: 7 stakow cobbla");
+					$sender->sendMessage("§7[ ========== §eCOBBLEX§7 ========== ]");
+					return true;
+				}
+					 if($args[0] == "kup") {
+					 if$sender->sendMessage("jejejejejrhrjrjrjrjr");
