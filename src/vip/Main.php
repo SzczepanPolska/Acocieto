31ffcfad2c7f44b35a1d9b963b08a5ee3d69c8f7
Szczepan Wąs <szczepan.was1@gmail.com>
2016-06-18 23:21:21 +0200

    Update Main.php

--- src/vip/Main.php
+++ src/vip/Main.php
@@ -12,18 +12,18 @@ use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase{
 	public function onEnable(){
-		$this->getLogger()->info("vipkup Zaladowane");
+		$this->getLogger()->info("svipkup Zaladowane");
 		}
 
   public function onCommand(CommandSender $sender, Command $command, $label, array $args){	
-			if(strtolower($command->getName()) === 'vip') {
+			if(strtolower($command->getName()) === 'D vip') {
 				if(empty($args)) {
 					$sender->sendMessage("•[§7==================§e[VIP]§7===================]•");
 					$sender->sendMessage("•§a Komendy: /back, /near, /time set [day] lub [night]");
-					$sender->sendMessage("•§a , /me");
-					$sender->sendMessage("•§a Itemy: żelazna zbroja oraz narzędzia 20 diax");
-					$sender->sendMessage("•§a 20 złota oraz 20 srebra");
-					$sender->sendMessage("•§a Oraz rangę §eVIP. By zakupić wpisz /vip kup!");
+					$sender->sendMessage("•§a , /me, /heal, /jump, /kit vip oraz §ePUPIL");
+					$sender->sendMessage("•§a Itemy: diax zbroja oraz narzędzia 40 diax");
+					$sender->sendMessage("•§a 40 złota oraz 40 srebra");
+					$sender->sendMessage("•§a Oraz rangę §eSVIP. By zakupić wpisz /svip kup!");
 					$sender->sendMessage("•[§7==================§e[VIP]§7==================]•");
 					return true;
 				}
@@ -33,7 +33,7 @@ class Main extends PluginBase{
                                         $sender->sendMessage("•§a o treści SIM.MINTS pod numer 7455");
                                         $sender->sendMessage("•§a koszt tylko 4.92zl! Otrzymany numer");
                                         $sender->sendMessage("•§a podaj tylko i wyłącznie Apple3910 oraz ThePlastus");
-                                        $sender->sendMessage("•§a Udanej zabawy z ranga VIP!");
+                                        $sender->sendMessage("•§a Udanej zabawy z ranga SVIP!");
                                 }
 	}
 }
