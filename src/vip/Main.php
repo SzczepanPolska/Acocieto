2449e5fceda93d7dfcb2ae2d823438052cc03458
Szczepan Wąs <szczepan.was1@gmail.com>
2016-06-19 08:56:21 +0200

    Update Main.php
    D

--- src/vip/Main.php
+++ src/vip/Main.php
@@ -24,15 +24,7 @@ class Main extends PluginBase{
 					$sender->sendMessage("•§a Itemy: diax zbroja oraz narzędzia 40 diax");
 					$sender->sendMessage("•§a 40 złota oraz 40 srebra");
 					$sender->sendMessage("•§a Oraz rangę §eSVIP. By zakupić wpisz /svip kup!");
-					$sender->sendMessage("•[§7==================§e[VIP]§7==================]•");
-					return true;
-				}
-					 if($args[0] == "kup") {
-                                        $sender->sendMessage("•§7[===================§a[ZAKUP]§7===================]");
-                                        $sender->sendMessage("•§a Aby zakupić §eVIP'a wyślij sms'a");
-                                        $sender->sendMessage("•§a o treści SIM.MINTS pod numer 7936");
-                                        $sender->sendMessage("•§a koszt tylko 11.07zl! Otrzymany numer");
-                                        $sender->sendMessage("•§a podaj tylko i wyłącznie Apple3910 oraz ThePlastus");
+					$sender->sendMessage("•[§ Apple3910 oraz ThePlastus");
                                         $sender->sendMessage("•§a Udanej zabawy z ranga SVIP!");
                                 }
 	}
